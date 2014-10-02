becd386a9cf33a0fa367fae985c1de05f93d0b46
Tan Tran <ttran@gamesalad.com>
2014-10-02 00:18:10 -0500

    Limit characters displayed

--- js/firebased.js
+++ js/firebased.js
@@ -1,19 +1,27 @@
 var myDataRef = new Firebase('https://resplendent-torch-6495.firebaseio.com/');
 
+var MAX_CHARS = 48;
+
 $('#letterInput').keypress(function (e) {
   if (e.keyCode == 13) {
     var letter = $('#letterInput').val();
     myDataRef.push({letter: letter});
+	var displayed_letters = $('#messages').children();
+	if (displayed_letters.length > MAX_CHARS) {
+		console.log(displayed_letters[0])
+		$(displayed_letters[0]).remove();
+	}
     $('#letterInput').val('');
   }
 });
 
-myDataRef.on('child_added', function(snapshot) {
+firstMessagesQuery = myDataRef.endAt().limit(MAX_CHARS);
+firstMessagesQuery.on('child_added', function(snapshot) {
   var message = snapshot.val();
   displayChatMessage(message.letter);
 });
 
 var displayChatMessage = function (letter) {
-  $('<span/>').text(letter).appendTo($('#messagesDiv'));
-  $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+  $('<span/>').text(letter).appendTo($('#messages'));
+  $('#messages')[0].scrollTop = $('#messages')[0].scrollHeight;
 };
