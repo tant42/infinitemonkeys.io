1880dd58b7764a400ded97e9ac2903aff8f3ee4e
Chris Boette <cboette@gmail.com>
2014-10-13 12:28:46 -0500

    Close issue #6 - add focus back to the input field after submission.

--- js/monkeys.js
+++ js/monkeys.js
@@ -1,6 +1,9 @@
 // GH Issue 3: Add cooldown period for after character input
 // https://github.com/tant42/infinitemonkeys.io/issues/3
 
+// GH Issue 6: Re-enable autofocus after submission
+// https://github.com/tant42/infinitemonkeys.io/issues/6
+
 $('form').submit(function(e){
   $('#letterInput').attr('disabled', true);
   $('#letterInput').css('border', '3px solid tomato');
@@ -10,5 +13,6 @@ $('form').submit(function(e){
 
 var reenable = function(){
   $('#letterInput').attr('disabled', false);
+  $('#letterInput').focus(); // sets the focus back to the input field
   $('#letterInput').css('border', '3px solid lime');
 };
